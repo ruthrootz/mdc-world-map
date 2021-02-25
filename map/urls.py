59089a8d5dda0e03b89bc0f4f26034d111fd68d5
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-24 22:00:30 -0500

    added marker file

--- map/urls.py
+++ map/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MapView
+from . views import *
 
 
 app_name = "map"
 urlpatterns = [
     path('', MapView.as_view()),
+    path('/marker', MarkerView.as_view()),
 ]
--- map/views.py
+++ map/views.py
@@ -2,4 +2,8 @@ from django.views.generic.base import TemplateView
 
 
 class MapView(TemplateView):
-    template_name = "map/map.html"
+    template_name = 'map/map.html'
+
+
+class MarkerView(TemplateView):
+    template_name = 'map/marker.html'
