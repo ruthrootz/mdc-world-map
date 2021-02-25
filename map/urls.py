42fdc16f24e6d273f858c66aa52040b21fe690eb
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-24 22:04:49 -0500

    leave your mark link works

--- map/urls.py
+++ map/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . views import *
 
 
-app_name = "map"
+app_name = 'map'
 urlpatterns = [
     path('', MapView.as_view()),
-    path('/marker', MarkerView.as_view()),
+    path('marker', MarkerView.as_view()),
 ]
