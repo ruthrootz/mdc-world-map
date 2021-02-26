4074359461b83434dc0709c7d409c9005b653857
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-26 15:58:20 -0500

    converted jQuery -> JS

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -19,19 +19,33 @@ ready(() => {
 
     let markers = [];
 
-    $.getJSON( '/locations', function(data) {
-        $.each(data, function() {
-            markers.push(
-                new google.maps.Marker({
-                    position: {
-                        lat: data['latitude'],
-                        lng: data['longitude'],
-                    },
-                    map: map,
-                    icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-                })
-            );
-        });
-    });
+    var request = new XMLHttpRequest();
+    request.open('GET', '/locations', true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            let data = JSON.parse(request.responseText);
+            data.forEach(function() {
+                markers.push(
+                    new google.maps.Marker({
+                        position: {
+                            lat: data['latitude'],
+                            lng: data['longitude'],
+                        },
+                        map: map,
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                        name: data['name'],
+                    })
+                );
+            });
+        } else {
+            console.error('ERROR: ' + request.error + ', ' + request.status);
+        }
+    };
+
+    request.onerror = function() {
+        console.error('ERROR: ' + request.error + ', ' + request.status);
+    };
+
+    request.send();
 
 });
