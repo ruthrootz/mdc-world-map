5796dde35e12804cc1cdb5459e12a2d84bcbe0ee
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-25 20:38:31 -0500

    removing old DB info

--- map/models.py
+++ map/models.py
@@ -1,4 +1,3 @@
-from django.contrib.gis.db.models import PointField
 from django.db import models
 
 
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -11,27 +11,27 @@ function ready(callback) {
 
 ready(() => {
 
-let map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(49.279504, -123.1162),
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-
-let markers = [];
-
-$.getJSON( '/locations', function(data) {
-    $.each(data, function() {
-        markers.push(
-            new google.maps.Marker({
-                position: {
-                    lat: data['location'][0],
-                    lng: data['location'][1]
-                },
-                map: map,
-                icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-            })
-        );
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(49.279504, -123.1162),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-});
+
+    let markers = [];
+
+    $.getJSON( '/locations', function(data) {
+        $.each(data, function() {
+            markers.push(
+                new google.maps.Marker({
+                    position: {
+                        lat: data['location'][0],
+                        lng: data['location'][1]
+                    },
+                    map: map,
+                    icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+                })
+            );
+        });
+    });
 
 });
