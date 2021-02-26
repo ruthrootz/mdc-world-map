90cb8ac19edec7925d4477b8e864d1d75c0795bf
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-25 21:07:38 -0500

    added Marker model

--- map/models.py
+++ map/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Marker(models.Model):
     name = models.CharField(max_length=255)
-    location = []
+    longitude = models.DecimalField(decimal_places=3, max_digits=25, default=0)
+    latitude = models.DecimalField(decimal_places=3, max_digits=25, default=0)
